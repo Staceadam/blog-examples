fd59c51bad179614ff920aef69a117282d33b957
Adam, Stacy <stacykelnhofer@northwesternmutual.com>
2021-05-18 08:12:53 -0500

    inits both rn projects

---
+++ react-native/micro-frontend/MicroFE/metro.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  ...require('./lib/engine/metro.config')
+}
---
+++ react-native/micro-frontend/MicroFE/react-native.config.js
@@ -0,0 +1,11 @@
+// react-native.config.js
+module.exports = {
+  project: {
+    android: {
+      sourceDir: 'lib/engine/android'
+    },
+    ios: {
+      project: 'lib/engine/ios/engine.xcodeproj'
+    }
+  }
+}
